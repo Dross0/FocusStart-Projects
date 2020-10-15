824a6c5b28e333a235cd5a30e3b72b081849d4ec
Андрей Гайдамака <andrew.gaidamaka@gmail.com>
2020-10-15 23:51:29 +0700

    Add table output. Finish task1

--- task1/src/main/java/ru/gaidamaka/MultiplicationTable.java
+++ task1/src/main/java/ru/gaidamaka/MultiplicationTable.java
@@ -16,10 +16,42 @@ public class MultiplicationTable {
         this.size = size;
     }
 
-    public void printTable(){
-    }
-
     private boolean validateSize(int size){
         return size >= MIN_TABLE_SIZE && size <= MAX_TABLE_SIZE;
+    }
+
+    public void printTable(){
+        final int fieldSize = WriterUtils.getSymbolsCountOfInteger(size * size);
+        final int factorFieldSize = WriterUtils.getSymbolsCountOfInteger(size);
+        for (int factor = 1; factor <= size; ++factor){
+            printRow(factor, fieldSize, factorFieldSize);
+            System.out.println();
+            printRowsSeparator(fieldSize, factorFieldSize);
+            System.out.println();
+        }
+    }
+
+    private void printRowsSeparator(int fieldSize, int factorFieldSize){
+        System.out.print(WriterUtils.getStringWithNSameChar('-', factorFieldSize) + "+");
+        String rowsSeparator = WriterUtils.getStringWithNSameChar('-', fieldSize);
+        for (int i = 1; i <= size; ++i){
+            if (i == size){
+                System.out.print(rowsSeparator);
+            }
+            else{
+                System.out.print(rowsSeparator + "+");
+            }
+        }
+    }
+
+    private void printRow(int factor, int fieldSize, int factorFieldSize) {
+        String format = "%" + fieldSize + "d|";
+        String firstElemFormat = "%" + factorFieldSize + "d|";
+        String lastElemFormat = "%" + fieldSize + "d";
+        System.out.printf(firstElemFormat, factor);
+        for (int i = 1; i < size; ++i){
+            System.out.printf(format, factor * i);
+        }
+        System.out.printf(lastElemFormat, factor * size);
     }
 }
