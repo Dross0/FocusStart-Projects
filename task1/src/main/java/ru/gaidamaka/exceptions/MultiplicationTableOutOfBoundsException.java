54832e424255f16307e56293e654d5a890937e6e
Андрей Гайдамака <andrew.gaidamaka@gmail.com>
2020-10-20 21:12:59 +0700

    Add Exceptions constructors with cause

--- task1/src/main/java/ru/gaidamaka/exceptions/MultiplicationTableOutOfBoundsException.java
+++ task1/src/main/java/ru/gaidamaka/exceptions/MultiplicationTableOutOfBoundsException.java
@@ -1,7 +1,15 @@
 package ru.gaidamaka.exceptions;
 
 public class MultiplicationTableOutOfBoundsException extends RuntimeException{
-    public MultiplicationTableOutOfBoundsException(final String errorMessage){
+    public MultiplicationTableOutOfBoundsException(String errorMessage){
         super(errorMessage);
+    }
+
+    public MultiplicationTableOutOfBoundsException(Throwable cause){
+        super(cause);
+    }
+
+    public MultiplicationTableOutOfBoundsException(String errorMessage, Throwable cause){
+        super(errorMessage, cause);
     }
 }
--- task1/src/main/java/ru/gaidamaka/exceptions/WrongTableSizeException.java
+++ task1/src/main/java/ru/gaidamaka/exceptions/WrongTableSizeException.java
@@ -4,4 +4,12 @@ public class WrongTableSizeException extends Exception{
     public WrongTableSizeException(String errorMessage){
         super(errorMessage);
     }
+
+    public WrongTableSizeException(String errorMessage, Throwable cause){
+        super(errorMessage, cause);
+    }
+
+    public WrongTableSizeException(Throwable cause){
+        super(cause);
+    }
 }
